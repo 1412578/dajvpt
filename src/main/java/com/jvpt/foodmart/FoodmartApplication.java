e097722a926c362e56442ade6841b6a82dfe9a95
Trần Quang Tri <tranquangtri96v2@gmail.com>
2017-10-26 14:14:47 +0700

    Tạo project mới

---
+++ src/main/java/com/jvpt/foodmart/FoodmartApplication.java
@@ -0,0 +1,12 @@
+package com.jvpt.foodmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodmartApplication.class, args);
+	}
+}
